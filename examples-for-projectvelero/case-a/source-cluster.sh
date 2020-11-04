593092830b2c8daa25d4ce66bbbd71b00103534c
Rafael Brito <rbrito@vmware.com>
2020-11-04 11:41:57 -0600

    Adding script on case a

--- examples-for-projectvelero/case-a/source-cluster.sh
+++ examples-for-projectvelero/case-a/source-cluster.sh
@@ -2,6 +2,7 @@
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml
 sleep 10
 kubectl apply --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/source/case-a-source.yaml
+sleep 10
 # creating the testing CRs
 kubectl create --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/source/music/config/samples/music_v1_rockband.yaml
 kubectl create --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/source/music/config/samples/music_v1alpha1_rockband.yaml
