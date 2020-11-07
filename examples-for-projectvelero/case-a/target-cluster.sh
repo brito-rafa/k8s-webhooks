25993f35716b12769795d0e6dc257946061470c5
Rafael Brito <rbrito@vmware.com>
2020-11-07 10:05:53 -0600

    Fixing bug on installation scripts

--- examples-for-projectvelero/case-a/target-cluster.sh
+++ examples-for-projectvelero/case-a/target-cluster.sh
@@ -5,7 +5,7 @@ sleep 20
 kubectl apply --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/target/case-a-target.yaml
 sleep 10
 echo "INFO: Waiting for controller to start..."
-while  [ "$(kubectl get pods -n music-system | grep -i running | grep '1/1' |  wc -l | awk '{print $1}')" != "1" ]; do echo "INFO: Waiting music-system..." && kubectl get pods -n music-system && sleep 10 ; done
+while  [ "$(kubectl get pods -n music-system | grep -i running | grep '2/2' |  wc -l | awk '{print $1}')" != "1" ]; do echo "INFO: Waiting music-system...  Break if it is taking too long..." && kubectl get pods -n music-system && sleep 10 ; done
 kubectl get pods -n music-system
 echo "INFO: Run a Velero Restore or create the testing CRs running:"
 echo "    kubectl create --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/target/music/config/samples/music_v1_rockband.yaml"
