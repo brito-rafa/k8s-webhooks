e334e03c9ccc9020f710217f43562f943e6e0203
Rafael Brito <rbrito@vmware.com>
2021-02-03 12:35:16 -0600

    Update target case-a script

--- examples-for-projectvelero/case-a/target-cluster.sh
+++ examples-for-projectvelero/case-a/target-cluster.sh
@@ -1,12 +1,7 @@
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml
-sleep 10
-echo "INFO: Waiting for cert-manager to start..."
-sleep 20
+while  [ "$(kubectl get pods -n cert-manager | grep -i running | grep '1/1' |  wc -l | awk '{print $1}')" != "3" ]; do echo "INFO: Waiting cert-manager..." && kubectl get pods -n cert-manager && sleep 10 ; done
 kubectl apply --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/target/case-a-target.yaml
-sleep 10
-echo "INFO: Waiting for controller to start..."
-while  [ "$(kubectl get pods -n music-system | grep -i running | grep '2/2' |  wc -l | awk '{print $1}')" != "1" ]; do echo "INFO: Waiting music-system...  Break if it is taking too long..." && kubectl get pods -n music-system && sleep 10 ; done
-kubectl get pods -n music-system
+while  [ "$(kubectl get pods -n music-system | grep -i running | grep '2/2' |  wc -l | awk '{print $1}')" != "1" ]; do echo "INFO: Waiting music-system..." && kubectl get pods -n music-system && sleep 10 ; done
 echo "INFO: Run a Velero Restore or create the testing CRs running:"
 echo "    kubectl create --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/target/music/config/samples/music_v1_rockband.yaml"
 echo "    kubectl create --validate=false -f https://raw.githubusercontent.com/brito-rafa/k8s-webhooks/master/examples-for-projectvelero/case-a/target/music/config/samples/music_v2beta1_rockband.yaml"
